b53b86f2117cbb8be07e1e79635a50a01268c549
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-02-24 22:02:31 -0300

    React basics - complete

--- 01ComoFunciona/ceep/src/App.js
+++ 01ComoFunciona/ceep/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
-import ListaDeNotas from "./components/listaDeNotas";
-import  FormularioCadastro  from "./components/formularioCadastro"; //temos que importar o nome da classe. E colocamos entre <> como uma tag.
+import ListaDeNotas from "./components/listaDeNotas/listaDeNotas";
+import  FormularioCadastro  from "./components/formularioCadastro/formularioCadastro"; //temos que importar o nome da classe. E colocamos entre <> como uma tag.
+import "./assets/App.css";
+import './assets/index.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
   render() {
     return (
-      <section>
-        <FormularioCadastro />
-        <ListaDeNotas />
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
