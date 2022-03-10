5726c091a4ccd0cf104c71abae6529181ad73f7b
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-03-09 22:20:38 -0300

    React Route - finished lesson 1

--- 03ReacRouter-SPA/petshop/src/App.jsx
+++ 03ReacRouter-SPA/petshop/src/App.jsx
@@ -1,25 +1,21 @@
+// Usamos o React Router Dom em: https://www.npmjs.com/package/react-router-dom
 import React from 'react'
 import './assets/css/base/base.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 
 function App() {
 
-  const Router = () => {
-    const location = window.location.pathname
-    if(location === '/sobre') { //na página inicial ("http://localhost:3000/") aparece só a página inicial
-      return <Sobre/> // No '/sobre/' ("http://localhost:3000/sobre"), aparece a página "sobre"
-    }
-    else {
-      return <Home/>
-    }
-  }
-
-  return (
-  <>
-    {Router()}
-  </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/sobre" element={<Sobre />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App
