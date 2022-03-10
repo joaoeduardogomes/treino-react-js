61b358a3a0eddacf1c75c30a97d8c06ff7e15710
João Gomes <joaoeduardo.gomes21@gmail.com>
2022-03-10 20:11:43 -0300

    React Router - finished lesson 3

--- 03ReacRouter-SPA/petshop/src/App.jsx
+++ 03ReacRouter-SPA/petshop/src/App.jsx
@@ -1,18 +1,24 @@
 // Usamos o React Router Dom em: https://www.npmjs.com/package/react-router-dom
 import React from 'react'
 import './assets/css/base/base.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
+import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
+import Post from './paginas/post';
 
 function App() {
 
     return (
         <Router>
+            <Cabecalho/>
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/sobre" element={<Sobre />} />
+                <Route path="/posts/:id" element={<Post />} />
+                <Route path="*" element={<Pagina404 />} /> {/* Qualquer caminho que não estiver nas rotas, cairá no erro por padrão */}
             </Routes>
         </Router>
     );
